a57a6afc7f85611de9b8e35b9e8193f0adced2e6
Tarun Bansal <officialtarunbansal@gmail.com>
2021-12-13 17:06:10 +0530

    channel added

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	gl "github.com/tarun4all/hotels-golang-app/pkg/geolocation"
 	parser "github.com/tarun4all/hotels-golang-app/pkg/importer/csv"
 	storage "github.com/tarun4all/hotels-golang-app/pkg/storage/mysql"
@@ -10,5 +12,36 @@ func main() {
 	storage := storage.New()
 	s := gl.NewService(storage)
 
-	parser.ParseCSV(s)
+	// channel for csv rows
+	readChannel := make(chan []string)
+
+	go func() {
+		var validRecord = 0
+		for data := range readChannel {
+			geolocation := gl.Geolocation{}
+			err := geolocation.Parse(data)
+
+			if err != nil {
+				continue
+			}
+
+			err = geolocation.ValidatePayload()
+
+			if err != nil {
+				continue
+			}
+
+			err = s.AddGeolocation(geolocation)
+
+			if err != nil {
+				continue
+			}
+
+			validRecord++
+		}
+
+		fmt.Printf("No. of Valid records : %v \n", validRecord)
+	}()
+
+	parser.ReadAllRecords(readChannel)
 }
--- pkg/importer/csv/parser.go
+++ pkg/importer/csv/parser.go
@@ -4,11 +4,9 @@ import (
 	"encoding/csv"
 	"fmt"
 	"os"
-
-	gl "github.com/tarun4all/hotels-golang-app/pkg/geolocation"
 )
 
-func ParseCSV(geolocationService *gl.GeolocationService) {
+func ReadAllRecords(readChannel chan []string) {
 	csvFile, err := os.Open("./data_dump.csv")
 	if err != nil {
 		fmt.Println(err)
@@ -23,33 +21,12 @@ func ParseCSV(geolocationService *gl.GeolocationService) {
 		fmt.Println("error ", err)
 	}
 
-	var validRecord = 0
-
 	// slice data as first row contains column name
 	for _, line := range records[1:] {
-		geolocation := gl.Geolocation{}
-		err := geolocation.Parse(line)
-
-		if err != nil {
-			continue
-		}
-
-		err = geolocation.ValidatePayload()
-
-		if err != nil {
-			continue
-		}
-
-		err = geolocationService.AddGeolocation(geolocation)
-
-		if err != nil {
-			continue
-		}
-
-		validRecord++
+		readChannel <- line
 	}
 
 	fmt.Printf("No. of Scanned records : %v \n", len(records))
-	fmt.Printf("No. of Valid records : %v \n", validRecord)
-	fmt.Printf("No. of Invalid record : %v \n", len(records)-validRecord)
+
+	close(readChannel)
 }
